69990dab9c94bbd36ecc4cc10ba9e25864098925
Divya Dharshan S <97381430+Divyadharshan@users.noreply.github.com>
2023-12-01 20:43:10 +0530

    Update DOA_01.py

--- December 01/DOA_01.py
+++ December 01/DOA_01.py
@@ -1,1 +1,9 @@
-
+n = int(input())
+runs = [int(i) for i in input().split()]
+maxi, t_runs = 0, 0
+for k in range(n):
+  if runs[k] > runs[maxi]:
+    maxi = k
+  t_runs += runs[k]
+print(t_runs)
+print(maxi)
