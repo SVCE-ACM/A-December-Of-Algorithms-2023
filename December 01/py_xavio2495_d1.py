d86b928e3d78a760a2cd92b24e3d1b1ca325c715
Immanuel <xavio.delta2495@gmail.com>
2023-12-01 18:47:17 +0530

    🚀Upload from xavio2495

---
+++ December 01/py_xavio2495_d1.py
@@ -0,0 +1,2 @@
+lst1 = [int(input("Enter score:")) for x in range(int(input("Enter the nos:")))]
+print("The player with highest score is:{}\nTeam scsore is:{}".format(lst1.index(max(lst1))+1,sum(lst1)))
