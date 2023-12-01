63ef040ad161efc3d1c9c6360e574dc2298caedf
Immanuel <xavio.delta2495@gmail.com>
2023-12-01 21:49:58 +0530

    🚀Uploaded Cricmetric

--- December 01/py_xavio2495_d1.py
+++ December 01/py_xavio2495_d1.py
@@ -1,2 +1,3 @@
+#December 1 - Cricmetric
 lst1 = [int(input("Enter score:")) for x in range(int(input("Enter the nos:")))]
 print("The player with highest score is:{}\nTeam scsore is:{}".format(lst1.index(max(lst1))+1,sum(lst1)))
