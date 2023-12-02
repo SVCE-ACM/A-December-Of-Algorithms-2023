5a4504c380dfed0f4b45acd9cf1099bbe5745ba9
Karthick K <karthick17702@gmail.com>
2023-12-02 14:00:00 +0530

    Adding second program.

---
+++ December 02/cpp_karthickk17.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    unordered_map<int,int> count;
+    for(int i=0;i<n;i++) {
+        cin>>arr[i];
+        count[arr[i]]++;
+    }
+    for(int i:arr){
+        if(count[i] > 0) {
+            cout<<count[i]<<" "; //inorder to preserve the order of appearance of elements, I have to mark the
+            //visited elements by making their count as 0.
+            count[i] = 0;
+        }
+    }
+    return 0;
+}
