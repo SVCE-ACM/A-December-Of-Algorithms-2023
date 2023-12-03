f6a18f8cf44d32dea2f59918ac2298215c316836
Immanuel <xavio.delta2495@gmail.com>
2023-12-03 09:40:51 +0530

    🚀Sunburnt

--- December 03/py_xavio2495_d3.py
+++ December 03/py_xavio2495_d3.py
@@ -1,5 +1,5 @@
 #December 03 - Sunburnt
-lst,c =[int(input("Enter height:")) for x in range(int(input("Enter the no. of buildings:")))],1
-m=lst[0]
+lst=[int(input("Enter height:")) for x in range(int(input("Enter the no. of buildings:")))]
+m,c=lst[0],1
 for x in lst:m,c= x if x>m else m,c+1 if x>m else c
 print("Building which can see the sunlight:",c)
