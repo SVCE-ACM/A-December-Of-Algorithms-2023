91cbed0bf6a0f914066ac7137c7ffbb9b5da5337
Karthick K <karthick17702@gmail.com>
2023-12-04 09:29:16 +0530

    Adding comment

--- December 04/cpp_karthickk17.cpp
+++ December 04/cpp_karthickk17.cpp
@@ -7,6 +7,8 @@ int main() {
     cin>>str;
     int n = str.length();
     if(n == 1) cout<<"Error";
+    //The smallest substing is of length 2 or 3. Its 2 in case of even length string and 3 in case of odd length string.
+    //All palindromic strings must have the same substring at the middle part. If it doesn't, it is not a palindrome.
     else if(n % 2 == 0){
         if(tolower(str[n/2]) == tolower(str[(n/2)-1]))
             cout<<str.substr((n/2)-1,2);
