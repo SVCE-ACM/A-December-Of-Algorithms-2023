6e39cd0b779d305b43d4efe52f9991af2a677936
Karthick K <karthick17702@gmail.com>
2023-12-05 09:25:41 +0530

    Adding fifth program

---
+++ December 05/cpp_karthickk17.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    int avg;
+    for (int i = 0; i < n; i++)
+        cin>>arr[i];
+    //The test cases doesn't provide correct answer for the given question. In the first test case
+    //the average is 30 and the sum of elements greater than or equal to average are 30 40 and 50 whose sum is 60.
+    avg=accumulate(arr.begin(),arr.end(),0)/n;
+    sort(arr.begin(),arr.end());
+    cout<<accumulate(lower_bound(arr.begin(),arr.end(),avg),arr.end(),0)<<endl;//The lower_bound() returns the iterator that
+    //points to the first element greater or equal to the value provided.
+    return 0;
+}
