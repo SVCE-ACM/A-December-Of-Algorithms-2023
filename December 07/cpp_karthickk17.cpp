751beb268b29493c86d9027e91b2ed88f475d47f
Karthick K <karthick17702@gmail.com>
2023-12-07 09:31:05 +0530

    Adding comment

--- December 07/cpp_karthickk17.cpp
+++ December 07/cpp_karthickk17.cpp
@@ -6,6 +6,6 @@ int main() {
     int width,height,radius;
     cin>>width>>height>>radius;
     float diagonal = sqrt(pow(width,2)+pow(height,2));
-    cout<<boolalpha<<(ceil(diagonal)<=(2*radius));
+    cout<<boolalpha<<(ceil(diagonal)<=(2*radius));//boolalpha sets the flag to display the boolean values as true or false.
     return 0;
 }
