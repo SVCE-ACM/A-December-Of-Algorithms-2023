e147ab7699df713eea10bdc3379672ef526f547e
Jeswin Paul Dany D <120293717+Jeswindany@users.noreply.github.com>
2023-12-13 18:45:27 +0530

    Add python3_Jeswindany_ForgotPassword.py

--- December 10/python3_Jeswindany_ForgotPassword.py
+++ December 10/python3_Jeswindany_ForgotPassword.py
@@ -1,9 +1,12 @@
+import re
+
 def substring(name, a, b):
     return name[a-1:a+b-1]
 
 data = [[100, "Shivnash Kumar"],[110,"Ragul Gupta"]]
 
-a,b= [int(i) for i in input().split()]
+ind = re.findall('\d+',input())
+a,b= [int(i) for i in ind]
 
 for i,j in data:
     print(substring(j,a,b))
