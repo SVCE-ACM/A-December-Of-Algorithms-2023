5f47db24bb7b6887653da0f9db73650d34a86123
Arudhran <72651851+ArudhranPK@users.noreply.github.com>
2023-12-20 18:21:43 +0530

    Create coder_of_conversions.py

---
+++ December 11/coder_of_conversions.py
@@ -0,0 +1,6 @@
+def sum_binary(x, y):
+    print(bin(x + y)[2::])
+
+x = int(input("Enter the 1st number to add and convert to binary : "))
+y = int(input("Enter the 2nd number to add and convert to binary : "))
+sum_binary(1, 3)
