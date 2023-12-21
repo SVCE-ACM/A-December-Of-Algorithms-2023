fd3a253ee8e88ec916af74448c3354715b36d483
Sunandita-R <sunanditar03@gmail.com>
2023-12-21 19:14:12 +0530

    Add files via upload

---
+++ December 11/c_Sunandita.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    printf("Enter two numbers separated by a space: ");
+    scanf("%d %d", &num1, &num2);
+
+    sum = num1 + num2;
+    printBinary(sum);
+    return 0;
+}
+void printBinary(int num) {
+    if (num == 0) {
+        printf("0");
+        return;
+    }
+
+    int binary[32];
+    int i = 0;
+    while (num > 0) {
+        binary[i] = num % 2;
+        num = num / 2;
+        i++;
+    }
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+
+    printf("\n");
+}
