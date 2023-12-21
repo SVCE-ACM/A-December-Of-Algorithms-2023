d18412453b817b2f8c2fc8baf63dfff836a64c4e
Sunandita-R <118152848+Sunandita-R@users.noreply.github.com>
2023-12-21 18:34:47 +0530

    Add files via upload

---
+++ December 10/java_Sunandita_ForgotPassword.java
@@ -0,0 +1,29 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class java_Sunandita_ForgotPassword {
+    public static void main(String[] args) {
+
+        String[] empname = {"Shivnash Kumar", "Ragul gupta", "Srikanth Kidambi"};
+
+        Scanner s = new Scanner(System.in);
+        String queryString = s.nextLine();
+        Pattern pattern = Pattern.compile("substring\\([^,]+,(\\d+),(\\d+)\\)");
+        Matcher matcher = pattern.matcher(queryString);
+
+        // Check if a match is found
+        if (matcher.find()) {
+            int firstNumber = Integer.parseInt(matcher.group(1));
+            int secondNumber = Integer.parseInt(matcher.group(2));
+
+            // Iterate through the array of employee names
+            for (int i = 0; i < empname.length; i++) {
+                int endIndex = Math.min(empname[i].length(), firstNumber - 1 + secondNumber);
+                System.out.println(empname[i].substring(firstNumber - 1, endIndex));
+            }
+        } else {
+            System.out.println("No match found.");
+        }
+    }
+}
