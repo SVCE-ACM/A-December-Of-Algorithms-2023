6f90ae85323ff259002b28d2f4dbc6ffddf4c714
Sunandita-R <sunanditar03@gmail.com>
2023-12-24 13:15:02 +0530

    Add files via upload

---
+++ December 24/c_Sunandita-R.c
@@ -0,0 +1,32 @@
+/*
+    ********        INPUT FORMAT EXAMPLE   ********
+    5
+    5 4 3 2 1
+*/
+
+#include <stdio.h>
+int countViolations(int array[], int n) {
+    int violations = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (array[i] > array[j]) {
+                violations++;
+            }
+        }
+    }
+
+    return violations;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    int array[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    int violations = countViolations(array, n);
+    printf("%d\n", violations);
+    return 0;
+}
