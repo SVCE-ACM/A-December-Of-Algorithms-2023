d62b4bd2916ab1f76143702503fec7a060306493
Hari Vignesh <131291876+HariVignesh18@users.noreply.github.com>
2023-12-29 22:32:00 +0530

    Update C++_HariVignesh18_PhantomCycle.cpp

--- December 26/C++_HariVignesh18_PhantomCycle.cpp
+++ December 26/C++_HariVignesh18_PhantomCycle.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<map>
 using namespace std;
+
 struct node
 {
     int val;
@@ -8,50 +9,52 @@ struct node
 };
 struct node *head = NULL;
 
-bool detectLoop(Node* head)
-    {
-        Node* temp = head;
-        map<Node*,bool> v;
-        while(temp!=NULL){
-            if(v[temp]!=true){
-            v[temp] = true;
-            temp = temp->next;}
-            else{
-                return true;
-            }
-        }
-        return false;
-}
-void insert(struct node *head, int val)
+bool detectLoop(struct node* head)
 {
-    struct node *nn = malloc(sizeof(struct node));
-    nn->val=val;
-    nn->next=NULL;
-    if(head==NULL){
-         head=nn;
-    }
-    else
-    {
-        struct node *t=head;
-        while(t->next!=NULL)
-        {
-            t=t->next;
+    struct node* temp = head;
+    map<node*,bool> v;
+    while(temp!=NULL){
+        if(v[temp]!=true){
+            v[temp] = true;
+            temp = temp->next;
         }
-        t->next=nn;
+        else{
+            return true;
+        }
+    }
+    return false;
+}
+
+void insert(int val) 
+{
+    struct node *nn = new struct node;
+    nn->val = val;
+    nn->next = NULL;
+    if (head == NULL) {
+        head = nn;
+    } else {
+        struct node *t = head;
+        while (t->next != NULL) {
+            t = t->next;
+        }
+        t->next = nn;
     }
 }
 
-int main(){
-  int n,e;
-  cin>>n;
-  for(int i=0; i<n; i++){
-    cin>>e;
-    insert(head,e);
-  }
-  if(detectLoop(head)){
-    cout << "Cycle Found";
-  }
-  else{
-    cout << "No Cycle Found";
-  }
+int main() {
+    int n, e;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> e;
+        insert(e);
+    }
+    // Adding a Loop
+    head->next->next->next = head;
+    if (detectLoop(head)) {
+        cout << "Cycle Found";
+    } else {
+        cout << "No Cycle Found";
+    }
+
+    return 0;
 }
