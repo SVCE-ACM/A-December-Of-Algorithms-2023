df71cb04b6bab1f178baa563f9f1fedb780f6b2e
ROHETH S <roheth1908@gmail.com>
2024-01-06 11:56:17 +0530

    Update dec10.cpp

--- December 10/dec10.cpp
+++ December 10/dec10.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 
 int main() {
-    set<string> tables{"emp"};//It is used for storing table names
-    unordered_map<string, vector<string>> table_column_map{{"emp",{"empname"}}};//It is used for storing the list of columns for the given table
-    unordered_map<string, vector<string>> column_values{{"empname",{"Shivnash Kumar", "Ragul Gupta"}}};//It is used for storing the list of columns for the given table
+    set<string> tables{"emp"};
+    unordered_map<string, vector<string>> table_column_map{{"emp",{"empname"}}};
+    unordered_map<string, vector<string>> column_values{{"empname",{"Shivnash Kumar", "Ragul Gupta"}}};
     string query, tableName, columnName;
     int length, startIndex;
     getline(cin, query);
-    //query = "select substring(empname,4,13) from emp;";
+    
     transform(query.begin(),query.end(),query.begin(),::tolower);
     regex pattern(R"(select\s+substring\((\w+),(\d+),(\d+)\)\s+from\s+(\w+);)");
 
-    // Match the regular expression pattern against the query
+    
     smatch matches;
     if (regex_match(query, matches, pattern)) {
         // Extract the matched components
         columnName = matches[1].str();
-        startIndex = stoi(matches[2].str())-1;  // Convert to int
-        length = stoi(matches[3].str());       // Convert to int
+        startIndex = stoi(matches[2].str())-1;  
+        length = stoi(matches[3].str());     
         tableName = matches[4].str();
     } 
     else {
