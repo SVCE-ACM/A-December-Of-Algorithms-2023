68870a85f8439f04d7a2dcb0f26932483b97caeb
CowTheGreat <98730541+CowTheGreat@users.noreply.github.com>
2024-01-12 23:49:00 +0530

    Add files via upload

---
+++ December 13/python_CowTheGreat_13.py
@@ -0,0 +1,25 @@
+decode = {
+    0:"none",
+    1:"none",
+    2:"ABC",
+    3:"DEF",
+    4:"GHI",
+    5:"JKL",
+    6:"MNO",
+    7:"PORS",
+    8:"TUV",
+    9:"WXYZ"
+}
+
+def textToNum(num):
+    for i in num:
+        if i.isalpha():
+            for j in decode.values():
+                if i in j:
+                    print(list(decode.keys())[list(decode.values()).index(j)],end="")
+        else:
+            print(i,end='')
+    print("\n")
+
+textToNum("123-647-EYES")
+textToNum("(325)444-TEST")
