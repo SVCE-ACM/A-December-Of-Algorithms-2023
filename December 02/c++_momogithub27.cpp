b9487b0b05b243c6d29ee2202f9378c8b6de4941
momogithub27 <155723188+momogithub27@users.noreply.github.com>
2024-01-14 18:48:08 +0530

    add December 1 - Cricmetric

---
+++ December 02/c++_momogithub27.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> v;
+unordered_map<int, int> filter(string s){
+    int temp = 0;
+    unordered_map<int, int> mp;
+    for(int i = 1; i<s.size(); i++){
+        if(s[i] >= '0' && s[i] <= '9'){
+            temp = (temp*10)+int(s[i] - '0');
+        } else {
+            mp[temp]++;
+            if(!count(v.begin(), v.end(), temp)) v.push_back(temp);
+            temp = 0;
+        }
+    }
+    return mp;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    unordered_map<int, int> mp = filter(s);
+    cout << '[';
+    for(int i = 0; i < v.size(); i++){
+        if(i == v.size()-1){
+            cout << mp[v[i]];
+            break;
+        }
+        cout << mp[v[i]] << ',';
+    }
+    cout <<  "]";
+    return 0;
+}
+
