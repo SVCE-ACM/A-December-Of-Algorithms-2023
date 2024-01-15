433b525d653df96d071da853af64e5c465297d8c
unKnown_NG <91405849+unKnownNG@users.noreply.github.com>
2024-01-15 20:31:06 +0530

    Add files via upload

---
+++ December 10/python3_unKnownNG_dec10.py
@@ -0,0 +1,8 @@
+start = int(input("Enter the starting index of the empname: "))
+skip = int(input("Enter the ending index of the empname: "))
+
+NAME1 = "Shivnash Kumar"
+NAME2 = "Ragul Gupta"
+
+print(NAME1[start-1:start + skip -1])
+print(NAME2[start-1:start + skip -1])
